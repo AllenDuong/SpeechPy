53988998ad6069a4d86115ea9f41e0ce219ea07e
aduong1 <aduong1@nd.edu>
2018-12-02 13:12:30 -0500

    Skeleton Update

--- Speech.py
+++ Speech.py
@@ -9,30 +9,81 @@ from pydub import AudioSegment
 ARGUMENTS   = sys.argv[1:]
 AUDIO_FILE	= ''
 OUTPUT_FILE = ''
+DURATION 	= 0
+OFFSET 		= 0
 
 # Functions
 def usage(exitCode = 0):
-    print('''Usage: {} [-a AudioFile -s BOOL -sn OutputFile]
-    -a   AudioFile 	File Path of Audio (.WAV or .FLAC)
-    -s   True/False 	Bool Whether to Save Output to File
-    -sn  OutputFile 	Name for Output File. If Empty then AudioFile.txt Used'''.format(os.path.basename(sys.argv[0])))
+    print('''Usage: {} [-a AudioFile -s BOOL ...]
+    -a   AudioFile  	File Path of Audio (.WAV or .FLAC)
+    -m   True			Use Microphone for Audio (Ignore File if Entered)
+    -s   True 		 	Bool Whether to Save Output to File (Default: False)
+    -sn  OutputFile 	Name for Output File. If Empty then AudioFile.txt Used
+    -d   LENGTH     	Duration of the Audio to be Processed
+    -o   OFFSET     	Starting Time (in Sec) to be Processed
+    -g   True 			Play "Guess the Word" Game?'''.format(os.path.basename(sys.argv[0])))
     sys.exit(exitCode)
 
-def audioRecognize():
+def recognizeFile():
+	# TODO: Use adjust_for_ambient_noise() or SciPy for Noise Reduction
+	pass
+
+	# Create Instance of Recognizer Class
+	r = sr.Recognizer()
+
 	# Create Instance of AudioData Class
 	name = sr.AudioFile(AUDIO_FILE)
 	with name as source: audio = r.record(source) # AudioData Object
 
-	# Create Instance of Recognizer Class - Subclass Google Cloud Speech
-	r = recognize_google_cloud(AUDIO_FILE)
+	# Analyze with Google Cloud Speech
+    response = { # Set Up Response Object
+        'success': True,
+        'error': None,
+        'transcription': None
+    }
+    try:
+		response['transcription'] = r.recognize_google_cloud(audio)
 
+	except sr.RequestError:
+		# API was Unreachable or Unresponsive
+		response['success'] = False
+        response['error'] = 'API Unavailable'
+
+    except sr.UnknownValueError:
+        # speech was Unintelligible
+        response["error"] = "Unable to Recognize Speech"
+
+    # Catch Errors
+    if response['error']:
+    	print('ERROR: {}'.format(response['error']))
+    	sys.exit(1)
+    	
 	# Check Whether to Save to File
 	if OUTPUT_FILE.empty():
 		pass
 	else:
-		with open(OUTPUT_FILE, "w") as output:
-		    output.write(r)
-		    
+		with open(OUTPUT_FILE, 'w') as output:
+		    output.write(script)
+
+def recognizeMic():
+	# Create Instance of Microphone Class
+	mic = sr.Microphone()
+
+	# Create Instance of Recognizer Class
+	r = Recognizer()
+
+	# Create Instance of AudioData Class
+	with mic as source:
+		r.adjust_for_ambient_noise(source)
+		audio = r.listen(source)
+
+	# Analyze with Google Cloud Speech
+
+	
+
+
+
+
 def convert2WAV():
 	pass
 
@@ -53,4 +104,4 @@ if __name__ == '__main__':
         else:
             usage(1)
 
-    # Perform Audio Analysis
+    # Check Parameter Values
